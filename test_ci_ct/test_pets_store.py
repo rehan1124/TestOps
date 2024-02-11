325cc81bb03c37dbb64faa009eb1c2e2fb3bcbee
Syed Rehan <jrehans@jci.com>
2024-02-11 13:07:07 +0530

    Test for store

--- test_ci_ct/test_pets_store.py
+++ test_ci_ct/test_pets_store.py
@@ -11,3 +11,7 @@ class TestPets(BaseTest):
     def test_find_pets_by_status(self, status):
         response = requests.get(url=self.base_url + self.find_by_status_url, params={"status": status})
         assert response.status_code == 200, "Incorrect HTTP code."
+
+    def test_get_store_inventory(self):
+        response = requests.get(url=self.base_url + self.store_get_inventory_url)
+        assert response.status_code == 200, "Incorrect HTTP code."
