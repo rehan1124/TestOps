edc6a8c51c28b8b50bfb8701d412127e12404d19
Syed Rehan <jrehans@jci.com>
2024-02-11 13:28:35 +0530

    Soft assertions with pytest_check

--- test_ci_ct/test_pets_store.py
+++ test_ci_ct/test_pets_store.py
@@ -1,4 +1,5 @@
 import pytest
+from pytest_check import check
 
 import requests
 
@@ -10,8 +11,17 @@ class TestPets(BaseTest):
     @pytest.mark.pets
     def test_find_pets_by_status(self, status):
         response = requests.get(url=self.base_url + self.find_by_status_url, params={"status": status})
-        assert response.status_code == 200, "Incorrect HTTP code."
+
+        with check:
+            assert response.status_code == 200, "Incorrect HTTP code."
 
     def test_get_store_inventory(self):
         response = requests.get(url=self.base_url + self.store_get_inventory_url)
-        assert response.status_code == 200, "Incorrect HTTP code."
+
+        with check:
+            assert response.status_code == 201, "Intentional fail."
+
+        print("--- Soft assertions in the test ---")
+
+        with check:
+            assert response.status_code == 202, "Incorrect HTTP code."
